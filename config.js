f55a716a92d1495d932805b60f6eea1233e8620f
dolphin-erp <byun7667@gmail.com>
2026-07-03 12:05:14 +0900

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@
 // 2) 배포된 웹앱 URL을 아래 API_URL에 붙여넣습니다.
 // 예: https://script.google.com/macros/s/AKfycbxxxxxxx/exec
 window.CONFIG = {
-  API_URL: 'PASTE_APPS_SCRIPT_WEBAPP_URL_HERE'
+  API_URL: 'https://script.google.com/macros/s/AKfycbyhys504lPWXdQsqgkbjEezu_w3XRcrQpyLvoQSy9FMy9Zm0zPtXIzvEvyCd-tM58-0/exec'
 };
